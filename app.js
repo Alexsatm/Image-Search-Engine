15f6acba94174a8c5b6780e9cc4011fa5d759466
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-12 22:58:07 +0300

    feat: add showMoreBtn listenner

--- app.js
+++ app.js
@@ -11,15 +11,16 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url)
     const data = await response.json()
 
     const results = data.results;
+
     results.map((result) => {
         const image = document.createElement('img');
-        image.scr = result.urls.small
+        image.src = result.urls.small
 
         const imageLink = document.createElement('a');
         imageLink.href = result.links.html;
@@ -28,10 +29,16 @@ async function searchImages() {
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink)
     })
+
+    showMoreBtn.style.display = 'block'
 }
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     page = 1;
     searchImages()
+})
+
+showMoreBtn.addEventListener('click', () => {
+    page++
 })
